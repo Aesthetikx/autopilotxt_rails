0bb734f3d7bd55669d6d2d0300ac1b79d8a377fe
Sydney Young <sydney.young@yale.edu>
2014-11-01 21:43:42 -0400

    Conversation validations

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,3 +1,18 @@
 class Conversation < ActiveRecord::Base
   has_many :messages
+  belongs_to :user
+
+  validates :expires_at, presence: true
+  validates :user_id, presence: true
+
+  scope :live, -> { where("expires_at > ?", Time.now) }
+  scope :expired, -> {where("expires_at < ?", Time.now) }
+
+  def live?
+    expires_at > Time.now
+  end
+  
+  def expired?
+    expires_at < Time.now
+  end
 end
