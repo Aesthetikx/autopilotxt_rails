f3aa071c80515867d8c7ac38336d1c54a693ce09
John DeSilva <desilvjo@umich.edu>
2014-11-02 02:23:03 -0500

    Add score specs for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,4 +18,36 @@ describe User do
     expect(FactoryGirl.build(:user)).to respond_to(:conversations)
   end
 
+  context 'score' do
+
+    let(:user) { FactoryGirl.create(:user) }
+
+    it 'has a score for responses' do
+      a = FactoryGirl.create(:response)
+      b = FactoryGirl.create(:response)
+
+      a.stub(:score).and_return(3)
+      b.stub(:score).and_return(2)
+
+      user.responses << a
+      user.responses << b
+
+      expect(user.response_score).to eq(5)
+    end
+
+    it 'has a score for conversations' do
+      a = FactoryGirl.create(:conversation)
+      b = FactoryGirl.create(:conversation)
+
+      a.stub(:score).and_return(4)
+      b.stub(:score).and_return(5)
+
+      user.conversations << a
+      user.conversations << b
+
+      expect(user.submission_score).to eq(4 + 5 + 2)
+    end
+
+  end
+
 end
