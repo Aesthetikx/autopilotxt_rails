9130350e53bc39153ef517d4ec3a6c0f1cd839b9
Sydney Young <sydney.young@yale.edu>
2014-11-01 21:04:27 -0400

    Message source is an enum

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,9 +1,11 @@
 class Message < ActiveRecord::Base
+  enum source: { uploader: 0, recipient: 1, autopilot: 2 }
+
+  belongs_to :conversation
+
   validates :body, length: { in: 1..350 }
   validates :body, presence: true
   validates :conversation_id, presence: true
   validates :source, presence: true
   validates :time, presence: true
-
-  belongs_to :conversation
 end
