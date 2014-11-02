d01c50058ac070e38c71649725c65ed11972da7f
Sydney Young <sydney.young@yale.edu>
2014-11-01 20:31:11 -0400

    Message validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,7 @@
 class Message < ActiveRecord::Base
+  validates :body, length: { in 1..350 }
+  validates :body, prescence: true
+  validates :conversation_id, prescence: true
+  validates :source, prescence: true
+  validates :time, prescence: true
 end
