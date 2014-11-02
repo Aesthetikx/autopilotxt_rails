7b39c9d5ba363fba59e6afaa9ed6cd236e802000
Sydney Young <sydney.young@yale.edu>
2014-11-01 21:12:49 -0400

    Added User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141102004209) do
+ActiveRecord::Schema.define(version: 20141102011114) do
 
   create_table "conversations", force: true do |t|
     t.integer  "user_id"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 20141102004209) do
     t.text     "body"
     t.integer  "source"
     t.datetime "time"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
