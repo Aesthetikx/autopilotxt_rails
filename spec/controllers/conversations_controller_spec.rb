b580ac8ce42d2c0e3a1573d3c7ba963f20e970ec
John DeSilva <desilvjo@umich.edu>
2014-11-01 23:13:44 -0400

    Dry conversations controller spec

--- spec/controllers/conversations_controller_spec.rb
+++ spec/controllers/conversations_controller_spec.rb
@@ -9,14 +9,44 @@ describe ConversationsController do
   end
 
   describe "GET /conversations/new" do
+    it 'sorts by created_at, newest first' do
+      conversations = [
+        FactoryGirl.create(:conversation, created_at: Time.now + 3.minutes),
+        FactoryGirl.create(:conversation, created_at: Time.now + 4.minutes),
+        FactoryGirl.create(:conversation, created_at: Time.now + 1.minutes)
+      ]
+      get :new
+      expect(assigns(:conversations).to_a).to eq(conversations.sort_by { |c| c.created_at })
+    end
+  end
 
+  describe "GET /conversations/old" do
+    it 'sorts by expired_at, closest first' do
+      conversations = [
+        FactoryGirl.create(:conversation, expires_at: Time.now + 3.minutes),
+        FactoryGirl.create(:conversation, expires_at: Time.now + 4.minutes),
+        FactoryGirl.create(:conversation, expires_at: Time.now + 1.minutes)
+      ]
+      get :new
+      expect(assigns(:conversations).to_a).to eq(conversations.sort_by { |c| c.expires_at })
+    end
+  end
+
+  describe "GET /conversations/hot" do
+    it 'sorts by votes, highest first'
+  end
+
+  describe "current" do
     it 'returns live conversations' do
       conversations = [
         FactoryGirl.create(:conversation, expires_at: Time.now - 3.minutes),
         FactoryGirl.create(:conversation, expires_at: Time.now + 3.minutes)
       ]
-      get :new
-      expect(assigns(:conversations)).to match_array(conversations.select(&:live?))
+
+      [:new, :old, :hot].each do |route|
+        get route
+        expect(assigns(:conversations)).to match_array(conversations.select(&:live?))
+      end
     end
 
     it 'returns conversations with at least 30 seconds remaining' do
@@ -24,40 +54,12 @@ describe ConversationsController do
         FactoryGirl.create(:conversation, expires_at: Time.now + 3.minutes),
         FactoryGirl.create(:conversation, expires_at: Time.now + 5.seconds)
       ]
-      get :new
-      expect(assigns(:conversations).sort).to match_array(conversations.select { |c| c.expires_at > Time.now + 30.seconds })
+
+      [:new, :old, :hot].each do |route|
+        get route
+        expect(assigns(:conversations).sort).to match_array(conversations.select { |c| c.expires_at > Time.now + 30.seconds })
+      end
     end
-
-    it 'sorts by expired_at, newest first' do
-      conversations = [
-        FactoryGirl.create(:conversation, expires_at: Time.now + 3.minutes),
-        FactoryGirl.create(:conversation, expires_at: Time.now + 4.minutes),
-        FactoryGirl.create(:conversation, expires_at: Time.now + 1.minutes)
-      ]
-      get :new
-      expect(assigns(:conversations)).to eq(conversations.sort_by { |c| c.expires_at })
-    end
-
-  end
-
-  describe "GET /conversations/old" do
-
-    it 'returns live conversations'
-
-    it 'returns conversations with at least 30 seconds remaining'
-
-    it 'sorts by expired_at, closest first'
-
-  end
-
-  describe "GET /conversations/hot" do
-
-    it 'returns live conversations'
-
-    it 'returns conversations with at least 30 seconds remaining'
-
-    it 'sorts by votes, highest first'
-
   end
 
 end
