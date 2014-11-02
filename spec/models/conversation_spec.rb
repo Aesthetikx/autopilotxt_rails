3057d4aa7f3684a9518505465902227218fc6694
John DeSilva <desilvjo@umich.edu>
2014-11-01 21:02:38 -0400

    Add live and expired specs to conversation

--- spec/models/conversation_spec.rb
+++ spec/models/conversation_spec.rb
@@ -18,4 +18,31 @@ describe Conversation do
     expect(FactoryGirl.create(:conversation, expires_at: nil)).not_to be_valid
   end
 
+  it 'is live if expiration date is in the future' do
+    conversation = FactoryGirl.create(:conversation, expires_at: Time.now + 5.minutes)
+    expect(conversation.live?).to be_false
+  end
+
+  it 'is expired if expiration date is in the past' do
+    conversation = FactoryGirl.create(:conversation, expires_at: Time.now - 5.minutes)
+    expect(conversation.expired?).to be_true
+  end
+
+  context 'scopes' do
+
+    let(:expired_conv) { FactoryGirl.create(:conversation, expires_at: Time.now - 5.minutes) }
+    let(:unexpired_conv) { FactoryGirl.create(:conversation, expires_at: Time.now + 5.minutes) }
+
+    it 'has and expired scope' do
+      expect(Conversation.expired).to include(expired_conv)
+      expect(Conversation.expired).not_to include(unexpired_conv)
+    end
+
+    it 'has a live scope' do
+      expect(Conversation.live).to include(unexpired_conv)
+      expect(Conversation.live).not_to include(expired_conv)
+    end
+
+  end
+
 end
