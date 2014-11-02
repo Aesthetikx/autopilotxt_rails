dc1bc3ea3d19cfb9bec1bd1185b8ddd578974442
Sydney Young <sydney.young@yale.edu>
2014-11-01 20:38:33 -0400

    Fix Message model typos

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ActiveRecord::Base
-  validates :body, length: { in 1..350 }
-  validates :body, prescence: true
-  validates :conversation_id, prescence: true
-  validates :source, prescence: true
-  validates :time, prescence: true
+  validates :body, length: { in: 1..350 }
+  validates :body, presence: true
+  validates :conversation_id, presence: true
+  validates :source, presence: true
+  validates :time, presence: true
 end
