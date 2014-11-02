e71e9432b7c738c794f7dc8c76e6b61985f884cc
John DeSilva <desilvjo@umich.edu>
2014-11-01 21:12:20 -0400

    Add initial user spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe User do
+
+  it 'has a valid factory' do
+    expect(FactoryGirl.build(:user)).to be_valid
+  end
+
+  it 'requires a username' do
+    expect(FactoryGirl.build(:user, username: nil)).not_to be_valid
+  end
+
+  it 'has a username' do
+    expect(FactoryGirl.build(:user)).not_to be_blank
+  end
+
+end
