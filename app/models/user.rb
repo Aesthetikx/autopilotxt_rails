2f0a85de16963621722a081e6add93b47d610320
Sydney Young <sydney.young@yale.edu>
2014-11-02 02:17:41 -0500

    User scores

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ActiveRecord::Base
   has_many :conversations
+  has_many :responses
 
   validates :username, presence: true
+
+  def submission_score
+    conversations.sent.map(&:score).inject(&:+) + conversations.size
+  end
+
+  def response_score
+    response.map(&:score).inject(&:+)
+  end
+
 end
