33ca3492acdf45371a5e2496f44ce8ad58b571a8
Sydney Young <sydney.young@yale.edu>
2014-11-01 22:12:57 -0400

    Conversations controller init

---
+++ app/controllers/conversations_controller.rb
@@ -0,0 +1,13 @@
+class ConversationsController < ApplicationController
+  def top
+  end
+
+  def new
+  end
+
+  def old
+  end
+
+  def hot
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'conversations/top'
+
+  get 'conversations/new'
+
+  get 'conversations/old'
+
+  get 'conversations/hot'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
