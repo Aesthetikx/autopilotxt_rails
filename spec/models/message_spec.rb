1b2998bc3be0095f4b320b2ddb1190550313cb6a
John DeSilva <desilvjo@umich.edu>
2014-11-01 20:16:27 -0400

    Write some specs

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,13 +2,25 @@ require 'spec_helper'
 
 describe Message do
 
-  it 'has a valid factory'
+  it 'has a valid factory' do
+    message = FactoryGirl.create(:message)
+    expect(message).to be_valid
+  end
 
-  it 'has a non empty body'
+  it 'has a non empty body' do
+    message = FactoryGirl.create(:message, body: "")
+    expect(message).not_to be_valid
+  end
 
-  it 'has a datestamp'
+  it 'has a datestamp' do
+    message = FactoryGirl.create(:message)
+    expect(message.time).to be_a(Datetime)
+  end
 
-  it 'belongs to a conversation'
+  it 'belongs to a conversation' do
+    message = FactoryGirl.create(:message)
+    expect(message.conversation).to be_a(Conversation)
+  end
 
   it 'has a source'
 
