397b580cad12dcaac2ea51fee2e7b6c758bdbcd8
John DeSilva <desilvjo@umich.edu>
2014-11-04 17:29:48 -0500

    Add empty response controller and spec

---
+++ spec/controllers/responses_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe ResponsesController do
+
+  it 'creates a response for a conversation'
+
+  it 'only allows a user to respond once per conversation'
+
+  it 'only allows a user to vote once per conversation'
+
+  it 'shows the top response for a conversation'
+
+  it 'shows all responses for a conversation'
+
+end
